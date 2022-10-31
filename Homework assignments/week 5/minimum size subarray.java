3b550b952f634d1ae38f4036ac99476d09790423
SNEHA V <107302558+sneha-venugopal@users.noreply.github.com>
2022-10-31 20:02:54 +0530

    Add files via upload

---
+++ Homework assignments/week 5/minimum size subarray.java
@@ -0,0 +1,11 @@
+    public int minSubArrayLen(int s, int[] A) {
+        int i = 0, n = A.length, res = n + 1;
+        for (int j = 0; j < n; ++j) {
+            s -= A[j];
+            while (s <= 0) {
+                res = Math.min(res, j - i + 1);
+                s += A[i++];
+            }
+        }
+        return res % (n + 1);
+    }
