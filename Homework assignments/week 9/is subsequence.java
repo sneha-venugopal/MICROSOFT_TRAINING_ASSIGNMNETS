e24adf9dd69a2cc86bce1edcf17aff3124c96c31
SNEHA V <107302558+sneha-venugopal@users.noreply.github.com>
2022-11-03 15:58:34 +0530

    Add files via upload

---
+++ Homework assignments/week 9/is subsequence.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        char cur[]=s.toCharArray();
+		//Initial value of past is -2 to avoid match at 0th index
+        int past=-2;
+        for(char c:cur){
+		//Every time we update the last match index +1 in past
+            past=t.indexOf(c,past+1);
+			//If no match found -1 will be returned,so returning false
+            if(past==-1)
+                return false;
+        }
+		//If the string s is traversed completely ,then return true
+        return true;
+    }
+}
